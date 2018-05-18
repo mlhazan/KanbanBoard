73445becc2a6c7b97900d9e51d30fb526b6c3532
mlhazan <mlhazan@hotmail.com>
2018-05-18 16:10:18 -0400

    API Added

---
+++ src/components/KanbanBoardContainer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import KanbanBoard from './KanbanBoard.jsx';
+
+
+const API_URL = 'http://kanbanapi.pro-react.com';
+const API_HEADERS = {
+    'content-type': 'application/json',
+    'Authorization': 'any-string-you-like'
+};
+
+class KanbanBoardContainer extends Component {
+    constructor(){
+        super(...arguments);
+        this.state = {
+            cards:[]
+        };
+    }
+    componentDidMount(){
+        fetch(API_URL + '/cards',{headers:API_HEADERS})
+            .then((response) =>response.json())
+            .then((responseDate) =>{
+                this.setState({cards:responseDate});
+            })
+            .catch((error) =>{
+                console.log('Error fetching and parsing data', error);
+            });
+    }
+    render(){
+        return <KanbanBoard cards = {this.state.cards}/>
+    }
+}
+export default KanbanBoardContainer;
